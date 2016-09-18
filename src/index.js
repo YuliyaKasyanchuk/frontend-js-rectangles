285b5777a06ec144f3bd70fad1c3668ba2828f32
YuliyaKasyanchuk <yuljakas@gmail.com>
2016-09-18 17:59:17 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -13,60 +13,56 @@ var rectangle1,rectangle2,rectangle3,rectangle4,rectangle5;
     height:25
  }
   rectangle3={
-    top:15,
-    left:10, 
+    top:10,
+    left:15, 
     width:20,
     height:20
  }
   rectangle4={
-    top:100,
+    top:10,
     left:10, 
+    width:0,
+    height:25
+ }
+  rectangle5={
+    top:10,
+    left:100, 
     width:5,
     height:5
  }
-  rectangle5={
-    top:26,
-    left:0, 
-    width:30,
-    height:10
- }
 
-   var x1=rectangle1.left;
-   var y1=rectangle1.top;
-   var x2=rectangle1.left+rectangle1.width;
-   var y2=rectangle1.top-rectangle1.height;
 
    var areIntersected=function(rectangle1,rectangle2){
-      var x3=rectangle2.left;
-      var y3=rectangle2.top;
-      var x4=rectangle2.left+rectangle2.width;
-      var y4=rectangle2.top-rectangle2.height;
+       var x1=rectangle1.left;
+       var y1=rectangle1.top;
+       var x2=rectangle1.left+rectangle1.width;
+       var y2=rectangle1.top-rectangle1.height;
+       var x3=rectangle2.left;
+       var y3=rectangle2.top;
+       var x4=rectangle2.left+rectangle2.width;
+       var y4=rectangle2.top-rectangle2.height;
 
       if(x3>=x2||x4<=x1||y3<=y2||y4>=y1) { 
-         return false;
+         return false;//не пересекаются
       }
       else{
          return true;
       }
     };
-
-console.log(areIntersected(rectangle1,rectangle2));
+console.log(areIntersected(rectangle1,rectangle2))
  
 var rectangles=[rectangle2,rectangle3,rectangle4,rectangle5];
-var filterVisible=[];
 
-for(var i=0;i<rectangles.length;i++){
-  if(areIntersected(rectangle1,rectangles[i])){
-      filterVisible.push(rectangles[i]);
+
+var filterVisible=function(rectangle1,rectangles){
+    var array=rectangles.filter(function(el){
+      if(areIntersected(rectangle1,el)&&(el.width*el.height!==0)){
+          return(el)
+        }
+      })
+    return(array);
   }
-  else{
-    continue;
-  }
-}
-
-console.log(filterVisible);
-
-
+console.log(filterVisible(rectangle1,rectangles))
 
 
 
